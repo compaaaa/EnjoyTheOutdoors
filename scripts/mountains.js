c04425366dfc6d3f8544206269cb91549b176aab
compaaaa <azi.varela2003@gmail.com>
2022-10-26 15:36:35 -0400

    added mountain images

---
+++ scripts/mountains.js
@@ -0,0 +1,12 @@
+function mountainCard(item) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = item.name;
+    card.innerHTML += `<img src="images/${item.img}">`;    
+    return card;
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    mountainsArray.forEach( m => document.body.appendChild(mountainCard(m)));
+    
+});
