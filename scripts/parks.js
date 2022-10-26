913318912a885f97845f1f201c94ec008262780e
compaaaa <azi.varela2003@gmail.com>
2022-10-26 15:00:08 -0400

    filter location & add bootstrap

--- scripts/parks.js
+++ scripts/parks.js
@@ -4,7 +4,7 @@ function locationOption(item){
     option.innerHTML = item;
     return option;
 }
-document.addEventListener("DOMContentLoaded", () => {
+/*document.addEventListener("DOMContentLoaded", () => {
 
     const select = document.getElementById("location");
     locationsArray.forEach(item => select.appendChild(locationOption(item)));
@@ -20,4 +20,29 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     const results = document.getElementById("results");
     nationalParksArray.forEach(item => results.appendChild(parkCard(item)));
+}); */
+
+function parkCard(item){
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = item.LocationName;
+    return card;
+}
+
+function showCards(list, target){
+    target.innerHTML = ""; //CLEAR
+    list.forEach(item => target.appendChild(parkCard(item)));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const results = document.getElementById("results");
+    const select = document.getElementById("location");
+    locationsArray.forEach(item => select.appendChild(locationOption(item)));
+    select.addEventListener("change", (e) => {
+        const v = select.selectedOptions[0].value;
+        const matches = nationalParksArray.filter(item=>item.State == v);
+        showCards(matches, results);
+    });
+    //show all cards
+    //showCards(nationalParksArray, results);
 });
