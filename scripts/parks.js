606cf9860612b66d0efb83ed411f91e2853dd30c
compaaaa <azi.varela2003@gmail.com>
2022-10-26 15:16:47 -0400

    filters both

--- scripts/parks.js
+++ scripts/parks.js
@@ -1,48 +1,42 @@
-function locationOption(item){
+function locationOption(item) {
     const option = document.createElement("option");
     option.value = item;
     option.innerHTML = item;
     return option;
 }
-/*document.addEventListener("DOMContentLoaded", () => {
 
-    const select = document.getElementById("location");
-    locationsArray.forEach(item => select.appendChild(locationOption(item)));
-    select.addEventListener("change", (e) => {
-        alert(select.selectedOptions[0].value)
-    })
-
-    function parkCard(item){
-        const card = document.createElement("div");
-        card.classList.add("card");
-        card.innerHTML = item.LocationName;
-        return card;
-    }
-    const results = document.getElementById("results");
-    nationalParksArray.forEach(item => results.appendChild(parkCard(item)));
-}); */
-
-function parkCard(item){
+function parkCard(item) {
     const card = document.createElement("div");
     card.classList.add("card");
     card.innerHTML = item.LocationName;
     return card;
 }
 
-function showCards(list, target){
-    target.innerHTML = ""; //CLEAR
+function showCards(list, target) {
+    target.innerHTML = ""; //clear
     list.forEach(item => target.appendChild(parkCard(item)));
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const results = document.getElementById("results");
     const select = document.getElementById("location");
-    locationsArray.forEach(item => select.appendChild(locationOption(item)));
-    select.addEventListener("change", (e) => {
+    const selectType = document.getElementById("type");
+    
+    function applyFilters() {
         const v = select.selectedOptions[0].value;
-        const matches = nationalParksArray.filter(item=>item.State == v);
-        showCards(matches, results);
-    });
+        const matches = nationalParksArray.filter(item => item.State == v || v == "");
+
+        const v2 = selectType.selectedOptions[0].value;
+        const matches2 = matches.filter(item => item.LocationName.toLowerCase().includes(v2.toLowerCase()) || v2 == "");
+
+        showCards(matches2, results);
+    }
+    locationsArray.forEach(item => select.appendChild(locationOption(item)));
+    parkTypesArray.forEach(item => selectType.appendChild(locationOption(item)));
+
+    select.addEventListener("change", applyFilters);
+    selectType.addEventListener("change", applyFilters);
+    
     //show all cards
     //showCards(nationalParksArray, results);
 });
